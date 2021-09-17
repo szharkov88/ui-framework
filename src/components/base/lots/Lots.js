0e4d1b08865be6c97560859bfd1bea69c5179d0b
szharkov <sharkov@tsconsulting.com>
2021-09-17 11:02:52 +0300

    edit store

--- src/components/base/lots/Lots.js
+++ src/components/base/lots/Lots.js
@@ -1,46 +1,14 @@
-import {api, stream} from "../../../utils/apiClients";
-import {state} from "../../../store";
-import {renderView} from "../../../index";
 import {Lot} from "../lot/Lot"
 import {Loading} from "../loading/Loading";
 
+
 export function Lots ({ lots }) {
-    api.get('/lots').then((lots) => {
-        state = {
-            ...state,
-            lots
-        }
-        renderView(state)
-
-        const onPrice = (data) => {
-            state = {
-                ...state,
-                lots: state.lots.map((lot) => {
-                    if (lot.id === data.id) {
-                        return {
-                            ...lot,
-                            price: data.price
-                        }
-                    }
-                    return lot
-                })
-            }
-            return renderView(state)
-        }
-
-        lots.forEach((lot) => {
-            stream.subscribe(`price-${lot.id}`, onPrice)
-        })
-    })
-
-
     if (!lots) {
         return {
             type: Loading,
             props: {}
         }
     }
-
     return {
         type: 'div',
         props: {
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,35 @@
+import {api, stream} from "../utils/apiClients";
+import {renderView} from "../index";
+
 export let state = {
     time: new Date(),
     lots: null
 }
+
+api.get('/lots').then((lots) => {
+    state = {
+        ...state,
+        lots
+    }
+    renderView(state)
+
+    const onPrice = (data) => {
+        state = {
+            ...state,
+            lots: state.lots.map((lot) => {
+                if (lot.id === data.id) {
+                    return {
+                        ...lot,
+                        price: data.price
+                    }
+                }
+                return lot
+            })
+        }
+        return renderView(state)
+    }
+
+    lots.forEach((lot) => {
+        stream.subscribe(`price-${lot.id}`, onPrice)
+    })
+})
